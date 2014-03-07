7c7eec4467b75cf07a469418ae030514d6f15050
StefanReinprecht <stefan.reinprecht@gmail.com>
2014-03-07 10:05:43 +0100

    Man kann selber Zeichnen aber die Koordinaten werden noch nicht getrakt

--- TuioSim/src/main/java/erkennung/InputField.java
+++ TuioSim/src/main/java/erkennung/InputField.java
@@ -5,24 +5,35 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 import javax.swing.JFrame;
 
 import game.Game;
 
-public class InputField extends JFrame{
-	
+public class InputField extends JFrame implements MouseMotionListener,
+		MouseListener, KeyListener {
+
 	private static final long serialVersionUID = 2399507823383479157L;
 	private Rectangle tuio;
+	private int difX;
+	private int difY;
 
-	public InputField(Game g){
+	public InputField(Game g) {
 		setTitle("Hier wird Gezeichnet");
 		setSize(450, 450);
 		setLocation(450, 0);
 		setVisible(false);
 		this.tuio = new Rectangle(50, 50, 40, 40);
+		addMouseListener(this);
+		addMouseMotionListener(this);
+		addKeyListener(this);
 	}
-	
+
 	public void paint(Graphics g) {
 		super.paint(g);
 		buildOrUpdateField(g);
@@ -40,5 +51,53 @@ public class InputField extends JFrame{
 		g2.draw(tuio);
 		g2.setColor(Color.white);
 		g2.fill(tuio);
+	}
+
+	public void updateLocation(MouseEvent e) {
+		tuio.setLocation(difX + e.getX(), difY + e.getY());
+		repaint();
+	}
+
+	public void keyTyped(KeyEvent e) {
+	}
+
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+			// isOnCard();
+		}
+	}
+
+	public void keyReleased(KeyEvent e) {
+	}
+
+	public void mouseClicked(MouseEvent e) {
+	}
+
+	public void mousePressed(MouseEvent e) {
+		difX = tuio.x - e.getX();
+		difY = tuio.y - e.getY();
+
+		if (tuio.contains(e.getX(), e.getY())) {
+			updateLocation(e);
+		}
+	}
+
+	public void mouseReleased(MouseEvent e) {
+		if (tuio.contains(e.getX(), e.getY())) {
+			updateLocation(e);
+		}
+	}
+
+	public void mouseEntered(MouseEvent e) {
+	}
+
+	public void mouseExited(MouseEvent e) {
+	}
+
+	public void mouseDragged(MouseEvent e) {
+		updateLocation(e);
+	}
+
+	public void mouseMoved(MouseEvent e) {
 	}
 }
